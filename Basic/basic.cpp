2df3335b4eed7a07996477a9e3b8d3fab6dc291f
MRIGAANKSh <mrigaanksharma928@gmail.com>
2024-12-31 14:10:03 +0530

    day1

---
+++ Basic/basic.cpp
@@ -0,0 +1,12 @@
+//multiple input 
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    int y;
+    cout << "enter "<< endl;
+    cin >> x >> y;
+    cout << x << y;
+    return 0;
+}
---
+++ Basic/input.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    cout << "enter " << endl;
+    cin >>x;
+    cout<< x;
+    return 0;
+}
