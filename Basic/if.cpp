5d2128d7a928fc100a48c3d3d412cc0359de8dcf
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-01-01 15:34:21 +0530

    done

---
+++ Basic/if.cpp
@@ -0,0 +1,17 @@
+//if else 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "enter age" << endl;
+    cin >> age;
+    if(age>18){
+        cout << "eligibile " << endl;
+
+    }
+    else{
+        cout << "not eligibile" <<endl ;
+    }
+}
