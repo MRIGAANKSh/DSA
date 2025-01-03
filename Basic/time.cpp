79facfe7b6515ae1b32efde1778949e44895f807
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-01-03 19:23:50 +0530

    tuf

---
+++ Basic/time.cpp
@@ -0,0 +1,13 @@
+//tuf sheet 
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "time complexity example";
+
+    for(int i=0;i<10;i++){
+        cout << i << endl;
+        
+    }
+    cout << "time complexity is O(n) loop run times--n \n";
+}
