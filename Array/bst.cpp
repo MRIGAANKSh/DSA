e6b7bc93de978c336b909e1ae02ffa9816a91433
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-01-15 19:07:53 +0530

    done

---
+++ Array/bst.cpp
@@ -0,0 +1,9 @@
+// Check for BST
+// Difficulty: EasyAccuracy: 25.37%Submissions: 551K+Points: 2
+// Given the root of a binary tree. Check whether it is a BST or not.
+// Note: We are considering that BSTs can not contain duplicate Nodes.
+// A BST is defined as follows:
+
+// The left subtree of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
