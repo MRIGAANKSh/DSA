0f3bb25a1af60c0f108d0aa27ad7cfb401ccb385
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-01-17 13:48:13 +0530

    array

---
+++ tufArray/largest.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    vector <int> arr={1,3,4,555,96,33,2,4,54,4,2,23};
+    sort(arr.begin(),arr.end());
+    cout << "greatest" << arr[arr.size()-1] << endl;
+    cout <<"second" << arr[arr.size()-2] << endl;
+}
