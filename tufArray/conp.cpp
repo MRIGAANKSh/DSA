505e944e1b38945e6de1ec60f2e2dc8dd1ddf092
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-01-28 17:00:39 +0530

    done

---
+++ tufArray/conp.cpp
@@ -0,0 +1,42 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool linearSearch(vector<int>&a, int num) {
+    int n = a.size(); //size of array
+    for (int i = 0; i < n; i++) {
+        if (a[i] == num)
+            return true;
+    }
+    return false;
+}
+int longestSuccessiveElements(vector<int>&a) {
+    int n = a.size(); //size of array
+    int longest = 1;
+    //pick a element and search for its
+    //consecutive numbers:
+    for (int i = 0; i < n; i++) {
+        int x = a[i];
+        int cnt = 1;
+        //search for consecutive numbers
+        //using linear search:
+        while (linearSearch(a, x + 1) == true) {
+            x += 1;
+            cnt += 1;
+        }
+
+        longest = max(longest, cnt);
+    }
+    return longest;
+}
+
+int main()
+{
+    vector<int> a = {100, 200, 1, 2, 3, 4};
+    int ans = longestSuccessiveElements(a);
+    cout << "The longest consecutive sequence is " << ans << "\n";
+    return 0;
+}
+
