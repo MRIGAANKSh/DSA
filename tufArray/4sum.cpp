6c2e24c489969750e53efc89f5f5eed67136d07c
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-02-02 14:17:47 +0530

    done dna done

---
+++ tufArray/4sum.cpp
@@ -0,0 +1,52 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    set<vector<int>> st;
+
+    //checking all possible quadruplets:
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            for (int k = j + 1; k < n; k++) {
+                for (int l = k + 1; l < n; l++) {
+                    // taking bigger data type
+                    // to avoid integer overflow:
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    sum += nums[l];
+
+                    if (sum == target) {
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                }
+            }
+        }
+    }
+    vector<vector<int>> ans(st.begin(), st.end());
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+    int target = 9;
+    vector<vector<int>> ans = fourSum(nums, target);
+    cout << "The quadruplets are: \n";
+    for (auto it : ans) {
+        cout << "[";
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
+
+
