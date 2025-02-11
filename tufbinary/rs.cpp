4cac7eea7d463258aff13f2f88896bd8622f32b8
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-02-11 13:51:10 +0530

    done

---
+++ tufbinary/rs.cpp
@@ -0,0 +1,24 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool searchInARotatedSortedArrayII(vector<int>&arr, int k) {
+    int n = arr.size(); // size of the array.
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == k) return true;
+    }
+    return false;
+}
+
+int main()
+{
+    vector<int> arr = {7, 8, 1, 2, 3, 3, 3, 4, 5, 6};
+    int k = 3;
+    bool ans = searchInARotatedSortedArrayII(arr, k);
+    if (!ans)
+        cout << "Target is not present.\n";
+    else
+        cout << "Target is present in the array.\n";
+    return 0;
+}
+
