c2d65578de7bef78de2d209cc979ce25b37ec6be
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-02-18 14:23:46 +0530

    done dna done

---
+++ tufbinary/smalldiv.cpp
@@ -0,0 +1,33 @@
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int smallestDivisor(vector<int>& arr, int limit) {
+    int n = arr.size(); //size of array.
+    //Find the maximum element:
+    int maxi = *max_element(arr.begin(), arr.end());
+
+    //Find the smallest divisor:
+    for (int d = 1; d <= maxi; d++) {
+        //Find the summation result:
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += ceil((double)(arr[i]) / (double)(d));
+        }
+        if (sum <= limit)
+            return d;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int limit = 8;
+    int ans = smallestDivisor(arr, limit);
+    cout << "The minimum divisor is: " << ans << "\n";
+    return 0;
+}
+
