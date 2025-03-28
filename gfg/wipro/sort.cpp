4b7624f031a49d6a91018923169faf6283bd4514
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-03-28 13:46:19 +0530

    done

---
+++ gfg/wipro/sort.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+      void sortArr(vector<int> &arr) {
+          // code here
+          sort(arr.begin(),arr.end());
+      }
+ };
+
+//   Sort The Array
+// Difficulty: BasicAccuracy: 41.02%Submissions: 124K+Points: 1
+// Given a random array arr of numbers, please return them in ascending sorted order. 
+// Note: Use Inbuilt functions to Sort the given array
+
+// Examples:
+
+// Input: arr[] = [1, 5, 3, 2]
+// Output: [1, 2, 3, 5]
+// Explanation: After sorting array will be like [1, 2, 3, 5].
