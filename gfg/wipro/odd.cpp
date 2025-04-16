ef79fa94d6a200fa3a6630aa4ee19a755176a31d
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-04-16 17:56:54 +0530

    done

---
+++ gfg/wipro/odd.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public:
+      int getSingle(vector<int>& arr) {
+          // code here]
+          int result = 0;
+          for (int num : arr) {
+              result ^= num;
+          }
+          return result;
+      }
+  };
