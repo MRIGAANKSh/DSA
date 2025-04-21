7187423e8092ef2004c351b0ccd8239067ddcd01
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-04-21 18:22:03 +0530

    done

---
+++ New folder/4.cpp
@@ -0,0 +1,10 @@
+class Solution {
+    public:
+      // Just print the space seperated array elements
+      void printArray(vector<int> &arr) {
+          // code here
+          for(int i=0;i<arr.size();i++){
+              cout << arr[i] << " ";
+          }
+      }
+  };
