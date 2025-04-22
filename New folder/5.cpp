bb8367ba29780557c483c9a3e47269a21e58176d
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-04-22 18:02:28 +0530

    done

---
+++ New folder/5.cpp
@@ -0,0 +1,10 @@
+class Solution {
+    public:
+      string toLower(string& s) {
+          // code here
+        for (int i = 0; i < s.length(); i++) {
+          s[i] = tolower(s[i]);
+      }
+      return s;
+      }
+  };
