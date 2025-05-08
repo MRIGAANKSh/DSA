00829241152523cc22de3f566bc9aa679cb56cf5
MRIGAANKSh <mrigaanksharma928@gmail.com>
2025-05-08 18:16:31 +0530

    :done

---
+++ Lovebabbar/1-2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=12;
+    char ch='2';
+    cout <<a<< endl;
+}
+
+//datatypes artihmetic...
+//typeof
+//typecasting
+//cout
+//etc
